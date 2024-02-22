5f096efe2a167036a080e4dc42c8d99da21afd96
Oscar Beckinger <144488958+OscarBeckinger@users.noreply.github.com>
2024-02-22 01:28:44 -0800

    Added hook to get truck reviews (#4)
    Added useGetTruckReviews which accepts a parameter truck name and only gets reviews for that specific truck. I also added a crude implementation of this hook in the food truck page for Thai8E8.

--- src/pages/trucks/Thai8E8/index.jsx
+++ src/pages/trucks/Thai8E8/index.jsx
@@ -1,9 +1,33 @@
 import ReviewInputBox from "../../../components/ReviewInputBox";
+import { useGetTruckReviews } from "../../../hooks/useGetTruckReviews";
 export const ThaiE = () => {
+    const reviews = useGetTruckReviews("8E8 Thai");
     return (
         <>
             <h1>Thai 8E8</h1>
             <ReviewInputBox associatedTruck={'8E8 Thai'}></ReviewInputBox>
+            <h2>Reviews: </h2>
+            <p>Review Count: {reviews.length}</p>
+            <div className="reviews">
+                <ul>
+                    {reviews.map((truckReview) => {
+                        const { name, profilePhoto, review, stars, title } = truckReview;
+                        return (
+                            <div className='list-item-div'>
+                                <li>
+                                    <div>
+                                        <h3>{name}</h3>
+                                        <img src={profilePhoto} alt="profile" height="50" width="50" />
+                                    </div>
+                                    <h3>{title}</h3>
+                                    <h5>Stars: {stars}</h5>
+                                    <p>{review}</p>
+                                    {/* <button onClick={() => { deleteFunction }}>Delete</button> (if we have time: only make this appear on reviews that match current user id (so only the person who posted the review can delete it)) */}
+                                </li>
+                            </div>);
+                    })}
+                </ul>
+            </div>
         </>
     );
 
