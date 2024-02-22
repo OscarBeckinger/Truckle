1e0f7cd5967fdb8f9151a0932a81374124b78a2b
Landan Devenecia <156151867+lando478@users.noreply.github.com>
2024-02-21 20:42:05 -0800

    Added delete review functionality. (#3)

--- src/hooks/useGetUserReviews.js
+++ src/hooks/useGetUserReviews.js
@@ -1,38 +1,36 @@
 import { db } from "../config/firebase-config";
 import { useState, useEffect } from "react";
-import { collection, getDocs, query, where } from "firebase/firestore";
+import { collection, query, where, onSnapshot } from "firebase/firestore";
 import { useGetUserInfo } from "./useGetUserInfo";
 
 export const useGetUserReviews = () => {
     const reviewsCollectionRef = collection(db, "reviews");
     const [userReviews, setuserReviews] = useState([]);
     const { userID } = useGetUserInfo(); 
-    const getUserReviews = async () => {
-        try {
-            let reviewsArr = [];
-            const q = query(collection(db, "reviews"), where("userID", "==", userID));
-            const userReviewsSnapshot = await getDocs(q);
-            userReviewsSnapshot.forEach(doc => {
+
+    useEffect(() => {
+        const q = query(collection(db, "reviews"), where("userID", "==", userID));
+        const unsubscribe = onSnapshot(q, (querySnapshot) => {
+            const reviewsArr = [];
+            querySnapshot.forEach((doc) => {
                 const reviewData = doc.data();
-                const { associatedTruck, createdAt, review, stars, title} = reviewData;
+                const { associatedTruck, createdAt, review, stars, title } = reviewData;
                 const reviewEntry = {
-                  associatedTruck,
-                  createdAt,
-                  review,
-                  stars,
-                  title
+                    id: doc.id,
+                    associatedTruck,
+                    createdAt,
+                    review,
+                    stars,
+                    title
                 };
                 reviewsArr.push(reviewEntry);
             });
             setuserReviews(reviewsArr);
-        } catch (err) {
-            console.error(err);
-        }
-    };
+        });
 
-    useEffect(() => {
-      getUserReviews();
-    }, []);
+        // Cleanup function to unsubscribe from the snapshot listener
+        return () => unsubscribe();
+    }, [userID]);
 
-    return { userReviews }
-}
+    return { userReviews };
+};
