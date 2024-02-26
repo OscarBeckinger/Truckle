2d82d6d1e6021c78d09d2d7a7dd3eb18f3c74371
OscarBeckinger <oscarbapps@gmail.com>
2024-02-25 23:56:52 -0800

    Added Salpicon page
    Added Salpicon page in src/pages/trucks/Salpicon and added basic html skeleton to test functionality

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Auth } from "./pages/auth";
 import { Homepage } from "./pages/home-page";
 import { ThaiE } from "./pages/trucks/Thai8E8";
 import { Perro } from "./pages/trucks/Perro";
+import { Salpicon } from "./pages/trucks/Salpicon";
 import AccountSettings from "./pages/account-settings/AccountSettings";
 
 
@@ -16,6 +17,7 @@ function App() {
           <Route path="/home-page" element={< Homepage />} />
           <Route path="/Thai8E8" element={< ThaiE />} />
           <Route path="/Perro" element={< Perro />} />
+          <Route path="/Salpicon" element={< Salpicon />} />
           <Route path="/AccountSettings" element={<AccountSettings />} /> {/* Add this route */}
         </Routes>
       </Router>
