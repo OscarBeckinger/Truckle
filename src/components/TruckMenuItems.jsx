7f1ef1b3a1fa9e34fc2d7f544ad476b8217a9089
Oscar Beckinger <144488958+OscarBeckinger@users.noreply.github.com>
2024-02-26 23:32:50 -0800

    Get menu items hook (#13)
    * Added Hooks for getting Menu Items
    
    Added Hook to get all menu items, and truck menu items, looking to combine into one hook. Also added small component for all menu items
    
    * Cleaned up get menu item hooks
    
    Removed unnecessary variables, console logs, etc. Also rearranged truck pages

---
+++ src/components/TruckMenuItems.jsx
@@ -0,0 +1,29 @@
+import { useGetTruckMenuItems } from "../hooks/useGetTruckMenuItems.js";
+const TruckMenuItems = ({ assocTruck }) => {
+    const items = useGetTruckMenuItems(assocTruck);
+    return (
+        <>
+            <div>Menu Items</div>
+            <div>
+                <ul>
+                    {items.map((item) => {
+                        const { associatedTruck, description, imageurl, title, } = item;
+                        return (
+                            <div>
+                                <li>
+                                    <img src={imageurl} alt="menu item" height="100" width="100" />
+                                    <h2>{title}</h2>
+                                    <h4>{associatedTruck}</h4>
+                                    <p>{description}</p>
+                                </li>
+                            </div>
+                        );
+                    })}
+                </ul>
+            </div>
+
+        </>
+    );
+
+}
+export default TruckMenuItems;
