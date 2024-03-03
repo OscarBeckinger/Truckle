9647826f0c00b9178988a316b5c23b1facd2835b
rudabasarwar <86314869+rudabasarwar@users.noreply.github.com>
2024-03-03 15:04:40 -0800

    imported food stickers (created them in Figma) and animation in css to make them shake around the logo and log-in button (#21)
    Co-authored-by: Rudaba Sarwar <rudabasarwar@ucla.edu>

--- src/pages/auth/index.jsx
+++ src/pages/auth/index.jsx
@@ -1,4 +1,12 @@
 import './login.css';
+import acaiBowlImage from '../../acaibowl.png';
+import burgerImage from '../../burger.png';
+import bulgogiRiceImage from '../../bulgogirice.png';
+import donutsImage from '../../donuts.png';
+import iceCreamImage from '../../icecream.png';
+import mangoRiceImage from '../../mangorice.png';
+import mangoSmoothieImage from '../../mangosmoothie.png';
+import tacoImage from '../../taco.png';
 import { auth, provider } from '../../config/firebase-config';
 import { signInWithPopup } from 'firebase/auth';
 import { useNavigate } from 'react-router-dom';
@@ -37,6 +45,31 @@ export const Auth = () => {
         <div className='auth-page'>
             <p>Login With Google Account</p>
             <button className="google-login-button" onClick={signInWithGoogle}>Sign In With Google</button>
+            
+            <div className="acaiBowl">
+                <img src = {acaiBowlImage} alt= "acai-bowl" className="images-d"/>
+            </div>
+            <div className="burger">
+                <img src = {burgerImage} alt= "burger" className="images-d"/>
+            </div>
+            <div className="bulgogiRice">
+                <img src = {bulgogiRiceImage} alt= "bulgogi-rice" className="images-d"/>
+            </div>
+            <div className="donuts">
+                <img src = {donutsImage} alt= "donuts" className="images-d"/>
+            </div>
+            <div className="iceCream">
+                <img src = {iceCreamImage} alt= "ice-cream" className="images-d"/>
+            </div>
+            <div className="mangoRice">
+                <img src = {mangoRiceImage} alt= "mango-rice" className="images-d"/>
+            </div>
+            <div className="mangoSmoothie">
+                <img src = {mangoSmoothieImage} alt= "mango-smoothie" className="images-d"/>
+            </div>
+            <div className="taco">
+                <img src = {tacoImage} alt= "taco" className="images-d"/>
+            </div>
         </div>
     );
 };
