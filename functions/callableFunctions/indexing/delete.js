0e641d5c0ce0431ccac91e9e1e53f3e8ce6ed8b2
OscarBeckinger <oscarbapps@gmail.com>
2024-03-03 22:51:34 -0800

    Added Meaningful Search and Firebase Functions
    Added Meaningful Search of Menu Items using Algolia API and Created Firebase Functions that update the corresponding Algolia index (adds record, deletes record, updates record) when the menuitems collection in the Firestore Database are modified (new document added, document deleted, document contents updated)

---
+++ functions/callableFunctions/indexing/delete.js
@@ -0,0 +1,22 @@
+const { ALGOLIA_ADMINKEY } = require("../../config");
+const { ALGOLIA_APPID } = require("../../config");
+
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+const algoliasearch = require("algoliasearch");
+if (admin.apps.length === 0) {
+    admin.initializeApp();
+}
+const ALGOLIA_APP_ID = ALGOLIA_APPID;
+const ALGOLIA_ADMIN_KEY = ALGOLIA_ADMINKEY;
+const ALGOLIA_INDEX_NAME = "menu_items";
+
+const client = algoliasearch(ALGOLIA_APP_ID, ALGOLIA_ADMIN_KEY);
+const index = client.initIndex(ALGOLIA_INDEX_NAME);
+
+exports.removeItemFromIndex = functions.firestore
+    .document("menuitems/{itemId}")
+    .onDelete((snap, context) => {
+        const itemId = context.params.itemId;
+        return index.deleteObject(itemId);
+    });
