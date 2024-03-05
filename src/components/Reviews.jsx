f07d8ee2f2476b05ce054f7c99ec95b5ce1f04e3
OscarBeckinger <oscarbapps@gmail.com>
2024-03-04 17:37:58 -0800

    Small Update to Reviews Component
    Updated Reviews Component and imported it in truck page so front end can start working on the stylling of reviews

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -1,20 +1,20 @@
 import React from "react";
 import "./review-box.css"
-const Review = ({name, profilePhoto, title, stars, review}) => {
+const Reviews = ({ name, profilePhoto, title, stars, review }) => {
     return (
-    <>
-    <div>
-        <h3>{name}</h3>
-        <img src={profilePhoto} alt="profile" height="50" width="50" />
-    </div>
-        <h3>{title}</h3>
-        <h5>Stars: {stars}</h5>
-        <p>{review}</p>
-    </>
+        <>
+            <div>
+                <h3>{name}</h3>
+                <img src={profilePhoto} alt="profile" height="50" width="50" />
+            </div>
+            <h3>{title}</h3>
+            <h5>Stars: {stars}</h5>
+            <p>{review}</p>
+        </>
     );
 
 }
 
-export default Review
+export default Reviews
 
 
