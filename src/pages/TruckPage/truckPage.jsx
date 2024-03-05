ce4dfc829d24ea6bb4fc89c3290e65e50a265d32
Oscar Beckinger <144488958+OscarBeckinger@users.noreply.github.com>
2024-03-04 17:38:23 -0800

    Small Update to Reviews Component (#33)
    Updated Reviews Component and imported it in truck page so front end can start working on the stylling of reviews

--- src/pages/TruckPage/truckPage.jsx
+++ src/pages/TruckPage/truckPage.jsx
@@ -1,6 +1,7 @@
 import ReviewInputBox from "../../components/ReviewInputBox";
 import CombinedMenuItems from "../../components/CombinedMenuItems";
 import { useGetTruckReviews } from "../../hooks/useGetTruckReviews";
+import Reviews from "../../components/Reviews";
 export const TruckPage = (truckName) => {
     const reviews = useGetTruckReviews(truckName);
     return (
@@ -18,13 +19,7 @@ export const TruckPage = (truckName) => {
                         return (
                             <div className='list-item-div'>
                                 <li>
-                                    <div>
-                                        <h3>{name}</h3>
-                                        <img src={profilePhoto} alt="profile" height="50" width="50" />
-                                    </div>
-                                    <h3>{title}</h3>
-                                    <h5>Stars: {stars}</h5>
-                                    <p>{review}</p>
+                                    <Reviews name={name} profilePhoto={profilePhoto} title={title} stars={stars}></Reviews>
                                     {/* <button onClick={() => { deleteFunction }}>Delete</button> (if we have time: only make this appear on reviews that match current user id (so only the person who posted the review can delete it)) */}
                                 </li>
                             </div>);
