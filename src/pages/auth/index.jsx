2062f023a0bfa23c9a8e97e250895b8a713f3a37
Rutvi Shah <rutvishah@Rutvis-MacBook-Air.local>
2024-03-04 16:08:56 -0800

    worked on homepage and login page

--- src/pages/auth/index.jsx
+++ src/pages/auth/index.jsx
@@ -1,7 +1,9 @@
 import './login.css';
-import { auth, provider } from '../../config/firebase-config';
-import { signInWithPopup } from 'firebase/auth';
+import logo from "../../TrucklyImage.png"
+import { auth, provider } from '../../config/firebase-config'
+import { signInWithPopup } from 'firebase/auth'
 import { useNavigate } from 'react-router-dom';
+
 
 export const Auth = () => {
     const navigate = useNavigate();
@@ -9,34 +11,41 @@ export const Auth = () => {
     const signInWithGoogle = async () => {
         try {
             const results = await signInWithPopup(auth, provider);
-            // Check if the user's email domain is @g.ucla.edu
-            if (results.user.email.endsWith('@g.ucla.edu')) {
-                const authInfo = {
-                    userID: results.user.uid,
-                    name: results.user.displayName,
-                    profilePhoto: results.user.photoURL,
-                    isAuth: true,
-                    email: results.user.email,
-                };
-                localStorage.setItem("auth", JSON.stringify(authInfo));
-                navigate("/home-page");
-            } else {
-                // Sign out the user if the email domain is not @g.ucla.edu
-                await auth.signOut();
-                alert('Please sign in with your @g.ucla.edu email.');
-            }
+
+            const authInfo = {
+                userID: results.user.uid,
+                name: results.user.displayName,
+                profilePhoto: results.user.photoURL,
+                isAuth: true,
+                email: results.user.email,
+            };
+            console.log(results.user.email);
+            console.log(results.user.displayName)
+            localStorage.setItem("auth", JSON.stringify(authInfo));
+            navigate("/home-page");
         } catch (error) {
             if (error.code === 'auth/cancelled-popup-request') {
                 // User cancelled the sign-in process
-                // console.log('User cancelled the sign-in process');
+                console.log('User cancelled the sign-in process');
             }
-        }
+        };
     };
-
     return (
         <div className='auth-page'>
             <p>Login With Google Account</p>
-            <button className="google-login-button" onClick={signInWithGoogle}>Sign In With Google</button>
-        </div>
-    );
-};
+            <button className="google-login-button" onClick={signInWithGoogle}> {" "} Sign In With Google</button>
+           
+            <div className="truckle-logo">
+                <img src = {logo} className ="logo"/>
+
+            </div>
+            
+         </div>
+        );  
+    };
+
+
+
+   
+ 
+        
--- src/pages/home-page/index.jsx
+++ src/pages/home-page/index.jsx
@@ -1,13 +1,21 @@
 import { useGetTrucks } from "../../hooks/useGetTrucks";
 import { useNavigate, Link } from "react-router-dom";
+import home from "../../TruckleImageSmall.png"
+import move from "../../movingTruck.png" 
+// import CombinedMenuItems from "../../components/CombinedMenuItems";
 import "./homepage.css";
 
+
+
+
+ 
 export const Homepage = () => {
     const { trucks } = useGetTrucks();
     const navigate = useNavigate();
     const handleClick = (navstr) => {
         // state object can contain any data you want to pass to the destination page.
         navigate(navstr);
+    
     };
 
     return (
@@ -15,22 +23,35 @@ export const Homepage = () => {
             {/* is this proper way to do navigation? */}
             <nav>
                 <Link to="/AccountSettings" className="accButton">Account Settings</Link>
-                <Link to="/LeaderBoard" >LeaderBoard</Link>
             </nav>
-
             <div className="homepage">
-                <div className="pageTitle">Home</div>
+               
+                                
+            
+                
+
+                <div className="truckle-home">
+                <img src = {home} className ="home"/></div>
+
+               
+
+                <div class="background-top"></div>
+                 <div class="background-bottom"></div>
+                                          
                 <div className="foodTruckList">
+
                     {trucks.map((truck, index) => {
-            <div className="truck-list-homepage">
-                <ul>
-                    {trucks.map((truck) => {
                         const { description, imageurl, title, navStr } = truck;
                         return (
                             <div className="foodTruckItem" key={index} onClick={() => handleClick(navStr)}>
                                 <div style={{ backgroundImage: `url(${imageurl})` }}></div>
                                  <h2 className="truckTitle">{title}</h2>
                                 <p>{description}</p>
+
+                                <div class="move-truck"></div>
+                                <img src = {move} className ="move"/>
+                                           
+                             
                             </div>
                         );
                     })}
