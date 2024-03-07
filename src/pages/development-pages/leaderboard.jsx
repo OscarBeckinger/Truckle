0b674cf07f9c86662192c0b15b63b66b682c9598
rudabasarwar <86314869+rudabasarwar@users.noreply.github.com>
2024-03-06 18:22:13 -0800

    Added styling to leaderboard page (#37)
    Co-authored-by: Rudaba Sarwar <rudabasarwar@ucla.edu>

--- src/pages/development-pages/leaderboard.jsx
+++ src/pages/development-pages/leaderboard.jsx
@@ -1,19 +1,36 @@
-import Board from "./board";
+import React from 'react';
+import Board from './board';
 import './style.css';
-import trophy from "../../Trophy.png"
-export default function Leaderboard(){
-      return(
-            <div className="App" id='main'> x
-       <Board />
-           
-            <div className="trophy-image">
-            <img src = {trophy} className ="trophy"/>
-      
-      
-             </div>
-        </div>
+import trophy from '../../Trophy.png';
+import confetti from 'https://cdn.skypack.dev/canvas-confetti';
 
+function makeConfetti() {
+    confetti({
+        particleCount: 100, // Number of confetti particles
+        spread: 300, // Spread of confetti particles
+        startVelocity: 50, // Initial velocity of confetti particles
+        ticks: 700, // Duration of confetti animation (increase to make it last longer)
+        origin: { y: 0.6 }, // Origin of confetti explosion
+        disableForReducedMotion: true, // Disable confetti animation for users with reduced motion preferences
+    });
 
-      );
-     
+    // Clear confetti after 5 seconds (adjust as needed)
+    setTimeout(() => {
+        confetti.reset();
+    }, 5000); // The time for how long you want the confetti to stay on the screen
+}
+
+export default function Leaderboard() {
+    makeConfetti(); // Call makeConfetti when component mounts
+
+      return (
+            <div className="background">
+                  <div className="App" id="main">
+                  <Board />
+                        <div className="trophy-image">
+                              <img src={trophy} className="trophy" alt="Trophy" />
+                        </div>
+                  </div>
+            </div>
+    );
 }
