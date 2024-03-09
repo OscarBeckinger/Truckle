965c312c164bd7c4bad8a181656f9c39a3228e12
RutviShah26 <157840854+RutviShah26@users.noreply.github.com>
2024-03-08 16:10:37 -0800

    worked on account setting page (#43)
    Co-authored-by: Rutvi Shah <rutvishah@Rutvis-MacBook-Air.local>

--- src/pages/account-settings/AccountSettings.jsx
+++ src/pages/account-settings/AccountSettings.jsx
@@ -1,17 +1,25 @@
 import React from 'react';
 import EditProfile from '../../components/EditProfile';
 import './account-settings.css';
+import banner from '../auth/assets/accountPage.png';
 // This is the Account Settings page, it displays eveyrthing related to you acc information:
 // name, email, all your reviews
 // AccountSettings has child views such as EditProfile
 const AccountSettings = () => {
   return (
+
+    
+
     <div className="background">
+    <div className="banner-account">
+    <img src = {banner} className ="banner"/>
+
     <div className="container">
       <h1 className="heading">Your Profile</h1>
       <div className="content">
         <EditProfile />
       </div>
+    </div>
     </div>
     </div>
   );
