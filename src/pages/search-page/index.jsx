03717ba2a63ef6613aadd8a91e9e50e70aceb0e3
Oscar Beckinger <144488958+OscarBeckinger@users.noreply.github.com>
2024-03-11 10:56:41 -0700

    Added styling to truck locations (#57)
    Added basic styling to truck locations component that fetches from ucla dining website

--- src/pages/search-page/index.jsx
+++ src/pages/search-page/index.jsx
@@ -2,14 +2,16 @@
 import Search from "../../components/Search";
 import TruckLocations from "../../components/TruckLocations";
 import Navbar from "../../components/Navbar";
+import "./styles.css";
 export const Searchpage = () => {
     return (
         <>
             <Navbar></Navbar>
             <Search></Search>
             <div height={100}></div>
-            <h2>Times/Locations</h2>
-            <TruckLocations></TruckLocations>
+            <div className="locations">
+                <TruckLocations></TruckLocations>
+            </div>
             {/*<CombinedMenuItems assocTruck={"None"}></CombinedMenuItems>*/}
         </>
     );
