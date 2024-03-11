59c5d8554c3d345f6cc6cc7e2e03b06ad3075489
RutviShah26 <157840854+RutviShah26@users.noreply.github.com>
2024-03-10 17:53:19 -0700

    fixed merge issues (#48)
    Co-authored-by: Rutvi Shah <rutvishah@Rutvis-Air.lan1>

--- src/pages/TruckPage/truckPage.jsx
+++ src/pages/TruckPage/truckPage.jsx
@@ -1,28 +1,19 @@
+import React from "react";
 import ReviewInputBox from "../../components/ReviewInputBox";
 import CombinedMenuItems from "../../components/CombinedMenuItems";
 import { useGetTruckReviews } from "../../hooks/useGetTruckReviews";
-import './truckPage.css';
 import Reviews from "../../components/Reviews";
-import "./truckPage.css"
-export const TruckPage = (truckName) => {
+import "./truckPage.css";
+
+export const TruckPage = (truckName) => { // Destructuring truckName from props object
     const reviews = useGetTruckReviews(truckName);
-    // this has the code where the menu item page is contained and everything 
+
     return (
         <>
-<<<<<<< HEAD
-
             <div className="center">
-                <h1 className="title">{truckName}</h1>
-                </div>
-            
-
-          
-=======
-            <div className="center">
-            <h1 className="menu-title">{truckName}</h1>
+                <h1 className="menu-title">{truckName}</h1>
             </div>
->>>>>>> main
-            {/*<TruckMenuItems assocTruck={truckName}></TruckMenuItems>*/}
+
             <CombinedMenuItems assocTruck={truckName}></CombinedMenuItems>
             <ReviewInputBox associatedTruck={truckName}></ReviewInputBox>
             <h2>Reviews: </h2>
@@ -32,16 +23,18 @@ export const TruckPage = (truckName) => {
                     {reviews.map((truckReview) => {
                         const { name, profilePhoto, review, stars, title } = truckReview;
                         return (
-                            <div className='list-item-div'>
+                            <div className='list-item-div' key={truckReview.id}>
                                 <li>
                                     <Reviews name={name} profilePhoto={profilePhoto} title={title} stars={stars}></Reviews>
                                     {/* <button onClick={() => { deleteFunction }}>Delete</button> (if we have time: only make this appear on reviews that match current user id (so only the person who posted the review can delete it)) */}
                                 </li>
-                            </div>);
+                            </div>
+                        );
                     })}
                 </ul>
             </div>
-            
         </>
     );
 };
+
+export default TruckPage;
