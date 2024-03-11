7e19a89d153b1d76c12c8ba65dbfa4231dd90675
Rutvi Shah <rutvishah@Rutvis-Air.lan1>
2024-03-10 17:51:53 -0700

    fixed merge issues

--- src/components/CombinedMenuItems.jsx
+++ src/components/CombinedMenuItems.jsx
@@ -1,53 +1,34 @@
 import React from "react";
 import { useGetMenuItemsCombined } from "../hooks/useGetMenuItems";
-import "./review-box.css"
-import "./menu.css"
-<<<<<<< HEAD
-=======
-
->>>>>>> main
+import "./review-box.css";
+import "./menu.css";
 
 const CombinedMenuItems = ({ assocTruck }) => {
     const items = useGetMenuItemsCombined(assocTruck);
 
     return (
         <>
-<<<<<<< HEAD
-                <div className="menupage">
-        
+            <div className="menupage">
                 <div className="heading">Menu Items</div>
-               
-                
-=======
-            <div className="heading">Menu Items </div>
-            <div>
-            
-                <ul>
->>>>>>> main
-                    {items.map((item, index) => {
-                        const { description, imageurl, title } = item;
-                        return (
-
-                            <div key={index} className="container-list">
-                                <li>
-                                    <div className="container">
-                                    <img src={imageurl} alt="menu item" height="100" width="100" />
-                                    <h2>{title}</h2>  
-                                    <p>{description}</p>
-                                    </div>
-                                </li>
-                            </div>
-                        );
-                    })}
-                
+                <div>
+                    <ul>
+                        {items.map((item, index) => {
+                            const { description, imageurl, title } = item;
+                            return (
+                                <div key={index} className="container-list">
+                                    <li>
+                                        <div className="container">
+                                            <img src={imageurl} alt="menu item" height="100" width="100" />
+                                            <h2>{title}</h2>  
+                                            <p>{description}</p>
+                                        </div>
+                                    </li>
+                                </div>
+                            );
+                        })}
+                    </ul>
+                </div>
             </div>
-<<<<<<< HEAD
-       
-                
-            
-=======
-           
->>>>>>> main
         </>
     );
 };
