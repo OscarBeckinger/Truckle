17e9375961ee4f9a3f9337507e6efa2d27fc646d
Oscar Beckinger <144488958+OscarBeckinger@users.noreply.github.com>
2024-03-10 20:20:55 -0700

    Added Nav bar, ASCII Conversion, Components (#51)
    * Fixed ASCII display
    
    Converted ASCII code to ASCII character when displaying truck locations
    
    * Added Search Item Component
    
    Added skeleton for searh item component, this will be used to style the menu items appearing when searching
    
    * Added Navbar and styling
    
    Added navbar to certain pages with routing and styling

--- src/pages/account-settings/AccountSettings.jsx
+++ src/pages/account-settings/AccountSettings.jsx
@@ -2,16 +2,15 @@ import React from 'react';
 import EditProfile from '../../components/EditProfile';
 import './account-settings.css';
 import banner from '../auth/assets/accountPage.png';
+import Navbar from '../../components/Navbar';
 // This is the Account Settings page, it displays eveyrthing related to you acc information:
 // name, email, all your reviews
 // AccountSettings has child views such as EditProfile
 const AccountSettings = () => {
   return (
-
-    
-
+    <>
+    <Navbar></Navbar>
     <div className="background">
-
       <div className="container">
         <div className="content">
           <EditProfile />
@@ -21,6 +20,7 @@ const AccountSettings = () => {
         <img src = {banner} className ="banner"/>
       </div>
     </div>
+    </>
   );
 };
 
