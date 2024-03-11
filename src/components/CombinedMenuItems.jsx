2ec44cff561e3e9d2c2ac2995ba5c69abbedac8c
RutviShah26 <157840854+RutviShah26@users.noreply.github.com>
2024-03-10 17:28:38 -0700

    Title position (#47)
    * worked on styling the menu page title
    
    * worked on styling title
    
    ---------
    
    Co-authored-by: Rutvi Shah <rutvishah@Rutvis-MacBook-Air.local>
    Co-authored-by: Rutvi Shah <rutvishah@Rutvis-Air.lan1>

--- src/components/CombinedMenuItems.jsx
+++ src/components/CombinedMenuItems.jsx
@@ -2,33 +2,52 @@ import React from "react";
 import { useGetMenuItemsCombined } from "../hooks/useGetMenuItems";
 import "./review-box.css"
 import "./menu.css"
+<<<<<<< HEAD
+=======
 
+>>>>>>> main
 
 const CombinedMenuItems = ({ assocTruck }) => {
     const items = useGetMenuItemsCombined(assocTruck);
 
     return (
         <>
+<<<<<<< HEAD
+                <div className="menupage">
+        
+                <div className="heading">Menu Items</div>
+               
+                
+=======
             <div className="heading">Menu Items </div>
             <div>
             
                 <ul>
+>>>>>>> main
                     {items.map((item, index) => {
                         const { description, imageurl, title } = item;
                         return (
-                            <div key={index}>
+
+                            <div key={index} className="container-list">
                                 <li>
+                                    <div className="container">
                                     <img src={imageurl} alt="menu item" height="100" width="100" />
-                                    <h2>{title}</h2>
-                                    
+                                    <h2>{title}</h2>  
                                     <p>{description}</p>
+                                    </div>
                                 </li>
                             </div>
                         );
                     })}
-                </ul>
+                
             </div>
+<<<<<<< HEAD
+       
+                
+            
+=======
            
+>>>>>>> main
         </>
     );
 };
