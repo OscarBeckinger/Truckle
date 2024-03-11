02b89d275fa88250b4484606b9ed995ee18e8ae8
Landan Devenecia <156151867+lando478@users.noreply.github.com>
2024-03-11 00:10:10 -0700

    add favorites component (#54)

---
+++ src/hooks/useGetFavorites.js
@@ -0,0 +1,44 @@
+import { useEffect, useState } from "react";
+import { query, collection, where, orderBy, onSnapshot } from "firebase/firestore";
+import { db } from "../config/firebase-config";
+//import { useGetUserInfo } from "./useGetUserInfo";
+
+export const useGetFavorites = (truckName) => {
+    const [truckReviews, setTruckReviews] = useState([]);
+    //const { userID } = useGetUserInfo(); (adding this makes other users profiles photos go away for some reason)
+    const reviewCollectionRef = collection(db, "reviews");
+    useEffect(() => {
+        const getTruckReviews = async () => {
+            let unsubscribe;
+            try {
+                const queryReviews = query(reviewCollectionRef, where("associatedTruck", "==", truckName), orderBy("createdAt"));
+                unsubscribe = onSnapshot(queryReviews, (snapshot) => {
+                    let docs = [];
+                    snapshot.forEach((doc) => {
+                        const reviewData = doc.data();
+                        const { createdAt, review, stars, title, profilePhoto, name } = reviewData;
+                        const reviewEntry = {
+                            id: doc.id,
+                            createdAt,
+                            review,
+                            stars,
+                            title,
+                            profilePhoto,
+                            name,
+                        };
+                        docs.push(reviewEntry);
+                    });
+                    setTruckReviews(docs);
+                });
+            } catch (error) { console.error(error) }
+            return () => unsubscribe();
+        };
+
+        getTruckReviews();
+
+        return () => { }; // cleanup function
+    }, [truckName]);
+
+    return { truckReviews };
+};
+
