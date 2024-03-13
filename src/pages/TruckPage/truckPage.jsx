947107705cdbb41e18412eb820aa4a4c7551ae6e
rudabasarwar <86314869+rudabasarwar@users.noreply.github.com>
2024-03-12 18:01:08 -0700

    add the background to TruckPage (#78)
    Co-authored-by: Rudaba Sarwar <rudabasarwar@ucla.edu>

--- src/pages/TruckPage/truckPage.jsx
+++ src/pages/TruckPage/truckPage.jsx
@@ -19,31 +19,35 @@ export const TruckPage = (truckName) => {
     };
 
     return (
-        <>
+        <div className="page-container">
             <Navbar></Navbar>
-            <div className="center">
-                <h1 className="menu-title">{truckName}</h1>
-            </div>
-            <CombinedMenuItems assocTruck={truckName}></CombinedMenuItems>
-            <ReviewInputBox associatedTruck={truckName}></ReviewInputBox>
-            <h2>Reviews </h2>
-            <div className="reviews-header">
-                <div className="scroll-buttons" style={{ textAlign: "right" }}> 
-                    <button onClick={() => scrollTo(-50)}>{"<"}</button>
-                    <button onClick={() => scrollTo(50)}>{">"}</button>
+            <div className="gradient-background">
+                <div className="center">
+                    <h1 className="menu-title">{truckName}</h1>
+                </div>
+                <div className="content-container">
+                    <CombinedMenuItems assocTruck={truckName}></CombinedMenuItems>
+                    <ReviewInputBox associatedTruck={truckName}></ReviewInputBox>
+                    <h2>Reviews </h2>
+                    <div className="reviews-header">
+                        <div className="scroll-buttons" style={{ textAlign: "right" }}> 
+                            <button onClick={() => scrollTo(-50)}>{"<"}</button>
+                            <button onClick={() => scrollTo(50)}>{">"}</button>
+                        </div>
+                    </div>
+                    <div className="reviews-container" ref={containerRef}>
+                        {reviews.map((truckReview, index) => {
+                            const { name, profilePhoto, review, stars, title } = truckReview;
+                            return (
+                                <div key={index} className="reviewContainer">
+                                    <Reviews name={name} profilePhoto={profilePhoto} title={title} stars={stars} review={review}></Reviews>
+                                </div>
+                            );
+                        })}
+                    </div>
                 </div>
             </div>
-            <div className="reviews-container" ref={containerRef}>
-                {reviews.map((truckReview, index) => {
-                    const { name, profilePhoto, review, stars, title } = truckReview;
-                    return (
-                        <div key={index} className="reviewContainer">
-                            <Reviews name={name} profilePhoto={profilePhoto} title={title} stars={stars} review={review}></Reviews>
-                        </div>
-                    );
-                })}
-            </div>
-        </>
+        </div>
     );
 };
 
