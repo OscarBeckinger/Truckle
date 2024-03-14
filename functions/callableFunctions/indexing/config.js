e8af11c36c89f5026890dde27e534eebcc323d9d
Landan Devenecia <156151867+lando478@users.noreply.github.com>
2024-03-13 19:42:52 -0700

    Modified Account Settings (#90)

---
+++ functions/callableFunctions/indexing/config.js
@@ -0,0 +1,4 @@
+import { algoliaAdminKey } from "../src/api";
+import { algoliaAppID } from "../src/api";
+module.exports.ALGOLIA_ADMINKEY = algoliaAdminKey;
+module.exports.ALGOLIA_APPID = algoliaAppID;
