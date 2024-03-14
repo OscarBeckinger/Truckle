86f7b139f01641d2101ca11b40b988cf64a69f90
joaquin <114783914+joaquinc777@users.noreply.github.com>
2024-03-14 16:45:40 -0700

    Menu page (#106)
    * display items horizontally
    
    * making items horizontally

--- src/components/CombinedMenuItems.jsx
+++ src/components/CombinedMenuItems.jsx
@@ -4,32 +4,37 @@ import "./review-box.css"
 import MenuItem from "./MenuItem"; 
 import "./menu.css";
 
-
 const CombinedMenuItems = ({ assocTruck }) => {
     const items = useGetMenuItemsCombined(assocTruck);
 
     return (
-        <>
-            
-                <div>
-                    <ul>
-                        {items.map((item, index) => {
-                            const { description, imageurl, title } = item;
-                            return (
-                                <div key={index} className="container-list">
-                                    <li>
-                                        <div className="menu-container">
-                                            <MenuItem imageurl={imageurl} title={title} description={description}></MenuItem> 
-                                        </div>
-                                    </li>
-                                </div>
-                            );
-                        })}
-                    </ul>
+<<<<<<< Updated upstream
+        <div className="container-list">
+        {items.map((item, index) => {
+            const { description, imageurl, title } = item;
+            return (
+                <div key={index} className="menu-container">
+                    <MenuItem imageurl={imageurl} title={title} description={description} />
                 </div>
-       
-        </>
-    );
+            );
+        })}
+    </div>
+);
+=======
+        <div className="foodItemsList"> 
+      {items.map((item, index) => {
+        const { description, imageurl, title } = item;
+        return (
+          <div key={index} className="foodItem"> 
+            <div style={{ backgroundImage: `url(${imageurl})` }}></div>
+            <h2>{title}</h2>
+            <p>{description}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
+>>>>>>> Stashed changes
 };
 
 export default CombinedMenuItems;
