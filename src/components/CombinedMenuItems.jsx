8682a4786e6c73282c13410e01e6997d0d50dbf3
joaquinc777 <114783914+joaquinc777@users.noreply.github.com>
2024-03-14 16:43:45 -0700

    making items horizontally

--- src/components/CombinedMenuItems.jsx
+++ src/components/CombinedMenuItems.jsx
@@ -4,11 +4,11 @@ import "./review-box.css"
 import MenuItem from "./MenuItem"; 
 import "./menu.css";
 
-
 const CombinedMenuItems = ({ assocTruck }) => {
     const items = useGetMenuItemsCombined(assocTruck);
 
     return (
+<<<<<<< Updated upstream
         <div className="container-list">
         {items.map((item, index) => {
             const { description, imageurl, title } = item;
@@ -20,6 +20,21 @@ const CombinedMenuItems = ({ assocTruck }) => {
         })}
     </div>
 );
+=======
+        <div className="foodItemsList"> 
+      {items.map((item, index) => {
+        const { description, imageurl, title } = item;
+        return (
+          <div key={index} className="foodItem"> 
+            <div style={{ backgroundImage: `url(${imageurl})` }}></div>
+            <h2>{title}</h2>
+            <p>{description}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
+>>>>>>> Stashed changes
 };
 
 export default CombinedMenuItems;
