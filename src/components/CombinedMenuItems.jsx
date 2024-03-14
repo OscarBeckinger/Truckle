6daf25d62bcebba1490a06d0339e3af5e38bc8b1
joaquinc777 <114783914+joaquinc777@users.noreply.github.com>
2024-03-14 16:49:36 -0700

    fixing merge conflict

--- src/components/CombinedMenuItems.jsx
+++ src/components/CombinedMenuItems.jsx
@@ -8,7 +8,6 @@ const CombinedMenuItems = ({ assocTruck }) => {
     const items = useGetMenuItemsCombined(assocTruck);
 
     return (
-<<<<<<< Updated upstream
         <div className="container-list">
         {items.map((item, index) => {
             const { description, imageurl, title } = item;
@@ -20,21 +19,6 @@ const CombinedMenuItems = ({ assocTruck }) => {
         })}
     </div>
 );
-=======
-        <div className="foodItemsList"> 
-      {items.map((item, index) => {
-        const { description, imageurl, title } = item;
-        return (
-          <div key={index} className="foodItem"> 
-            <div style={{ backgroundImage: `url(${imageurl})` }}></div>
-            <h2>{title}</h2>
-            <p>{description}</p>
-          </div>
-        );
-      })}
-    </div>
-  );
->>>>>>> Stashed changes
 };
 
 export default CombinedMenuItems;
