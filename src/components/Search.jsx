f7b706e604fc76eb9aa4ec594d784973db853fa6
Rutvi Shah <rutvishah@Rutvis-MacBook-Air.local>
2024-03-13 17:19:58 -0700

    chnaged styling for sccount settings page

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -6,6 +6,7 @@ import { algoliaAppID } from "../api";
 import "./search.css";
 
 const Search = () => {
+    
     const [searchClient, setSearchClient] = useState(null);
     useEffect(() => {
         setSearchClient(algoliasearch(algoliaAppID, 'ee22c38b9d035ce5b9ad0e4a4f4689bd')); //public keys (admin key is only one you really need private, but we keep App ID private for extra security)
@@ -14,22 +15,33 @@ const Search = () => {
     const Hit = ({ hit }) => {
         return (
             <div>
+                 
                 <SearchMenuItem hit={hit}></SearchMenuItem>
             </div>
+          
         );
     }
 
     return (
-        <div>
+        <div> 
             {searchClient &&
                 <div>
                     <InstantSearch searchClient={searchClient} indexName="menu_items">
                         <SearchBox className="search-button svg" placeholder="Search Menu Items" searchAsYouType={true} />
                         <Hits hitComponent={Hit} />
-                    </InstantSearch>
+                       
+
+                    </InstantSearch>   
+                
+                   
                 </div>
+             
             }
+           
         </div>
+         
+       
+        
     );
 }
 
