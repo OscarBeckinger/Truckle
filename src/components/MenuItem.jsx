0be3630249af6de619ab030ea6ee6c21d282fae9
Rudaba Sarwar <rudabasarwar@ucla.edu>
2024-03-13 17:40:55 -0700

    adding popup in TruckPage, hover on foodItems

--- src/components/MenuItem.jsx
+++ src/components/MenuItem.jsx
@@ -1,12 +1,14 @@
 import React from "react";
-const MenuItem = ({ imageurl, title, description }) => {
-    return (
-        <>
-            <img src={imageurl} alt="menu item" height="100" width="100" />
-            <h2>{title}</h2>         
-            <p>{description}</p>
-        </>
-    );
+import './menu.css';
 
+const MenuItem = ({ imageurl, title, description }) => {
+   return (
+       <>
+      
+           <img src={imageurl} alt="menu item" height="100" width="100" />
+           <h2>{title}</h2>        
+           <p>{description}</p>
+       </>
+   );
 }
 export default MenuItem
