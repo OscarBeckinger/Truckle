b645f7947bc53d19d38298abc22be246868ab1f3
Rudaba Sarwar <rudabasarwar@ucla.edu>
2024-03-14 00:47:52 -0700

    updated searchpage styling

--- src/pages/search-page/index.jsx
+++ src/pages/search-page/index.jsx
@@ -8,14 +8,12 @@ import "./styles.css";
 export const Searchpage = () => {
     return (
         <div className="scontainer">
-            <Navbar />
             <div className="background"> 
                 <div className="scontent">
                     <Search />
                     <div style={{ minHeight: "100px" }}></div>
                 </div>
             </div>
-            <Footer />
         </div>
     );
 }
